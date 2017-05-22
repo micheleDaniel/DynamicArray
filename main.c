0fdea7658e78ca18156e55d4d0eafe942f1d10fd
michel <ken.hollandes@gmail.com>
2017-05-22 18:08:36 +0200

    ho occcupato la dimensione

--- main.c
+++ main.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/* ARRAY DYMANIC
+------------------
+*/
 void line();
+void trattino();
 
-int main(){
-	printf("Hello, World");
-	line();
-	return 0;
+int main() {
+  int *array ;
+  int d;
+
+  printf("inserisci dimensione: ");
+  scanf("%d", &d);
+
+  array=(int *)malloc(sizeof(int)*d);
+  
+  printf("la tua dimensione e'': %d", d);
+  line();
+  trattino();
+  
+  
+  getchar();
+  system("pause");
+  line();
+  return 0;
 }
-
+		
 void line(){
 	printf("\n");
+	
+}
+
+void trattino(){
+	printf("------------------------------\n");
 }
