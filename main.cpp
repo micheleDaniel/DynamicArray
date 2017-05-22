801e6bfc1a30992294f02f6ff9501b70eb507005
c0desBym3ta <m3tahckr@hotmail.com>
2017-05-22 18:37:20 +0000

    Printing Elements of array and addind line function

--- main.cpp
+++ main.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 #include <string>
 
+void line();
+
 int main(){
 	int *A,N;
 	
+	//We must not have Negative value in dimension. So use do-while.
 	do{
 		std::cout << "Insert Array Dimension: ";
 		std::cin >> N;
 	}while(N<=0);
 	
+	//Occupying new space for out array.
 	A = new int[N];		
 	
+	//Insert Array Elements.
+	line();
 	for(int i = 0; i<N; i++){
 		std::cout <<"Insert Array Elements A["<<i<<"]: ";
 		std::cin >> A[i];
 	}
 
+	//Print Array elements.
+	std::cout <<"\nArray: "; //Outside from main because it must be printed only one time.
+	for(int i = 0; i<N; i++){
+		std::cout<<"["<<A[i]<<"]";
+	}
+	
+	line();
 	return 0;
 }
 
+void line(){
+	std::cout << std::endl;
+}
