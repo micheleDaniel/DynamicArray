abb15946ac7af804ffe7cab9700ab1debd658d78
c0desBym3ta <m3tahckr@hotmail.com>
2017-05-22 18:23:31 +0000

    Insert elements in Array

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include <string>
 
 int main(){
+	int *A,N;
+	
+	do{
+		std::cout << "Insert Array Dimension: ";
+		std::cin >> N;
+	}while(N<=0);
+	
+	A = new int[N];		
+	
+	for(int i = 0; i<N; i++){
+		std::cout <<"Insert Array Elements A["<<i<<"]: ";
+		std::cin >> A[i];
+	}
 
-	std::cout << "Hello, World" <<std::endl;
 	return 0;
 }
 
